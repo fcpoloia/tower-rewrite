4a5c19f20204e867bed3104081d3d00d11b54a15
fcpoloia <z21comma@proton.me>
2024-12-13 22:18:20 +0100

    Added -options - folder for everything configuration related -keymaps.json - stores pygames key variables (K_) as map's key and a string as value -loadInputMap() - function that reads keymaps.json and translates the values of the map into function names -Player.shoot() - a Player function that initializes a bullet -Player.shoot() - a Player function that initializes a bullet -Player.BGroup - A bullet group stored as a variable in Player class] Modified -Player.init() - now takes bullet group as an argument -Player.update() - loads mouse position into a variable every tick -Player.rotate() - no longer takes

--- lib/Player.py
+++ lib/Player.py
@@ -1,9 +1,11 @@
 import pygame
 import math
 
+from lib.Bullet import Bullet
+
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos: tuple[int, int]) -> None:
+    def __init__(self, pos: tuple[int, int], bulletGroup: pygame.sprite.Group) -> None:
         super().__init__()
         self.image_original = pygame.image.load("img/PlayerImage.png")
         self.image_original = pygame.transform.scale(self.image_original, (50, 50))
@@ -11,12 +13,27 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
-    def rotate(self, mpos: tuple):
-        dx = mpos[0] - self.rect.centerx
-        dy = mpos[1] - self.rect.centery
+        self.BSpeed = 10
+
+        # Imported
+        self.mpos = (0, 0)
+        self.BGroup = bulletGroup
+
+    def shoot(self):
+        self.BGroup.add(Bullet(self.rect.center, self.mpos, self.BSpeed))
+
+    def rotate(self):
+        dx = self.mpos[0] - self.rect.centerx
+        dy = self.mpos[1] - self.rect.centery
         angle = math.degrees(math.atan2(-dy, dx))
         self.image = pygame.transform.rotate(self.image_original, (angle - 90))
         self.rect = self.image.get_rect(center=self.rect.center)
 
     def update(self, mpos: tuple) -> None:
-        self.rotate(mpos)
+        """
+        UPDATE variables and state of the player
+
+        mpos - mouse position, it's better to keep it in `self` than for every function to need it as argument
+        """
+        self.mpos = mpos
+        self.rotate()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+import sys
 import pygame
-from lib.Bullet import Bullet
 from lib.Player import Player
 
 # Initialize components and constants
@@ -13,10 +14,32 @@ FPS = 60
 BG = (60, 56, 54)
 
 # Player initialization
-player = Player((WIDHT // 2, HEIGHT // 2))
+bulletGroup = pygame.sprite.Group()
+player = Player((WIDHT // 2, HEIGHT // 2), bulletGroup)
 playerGroup = pygame.sprite.Group()
 playerGroup.add(player)
-bulletGroup = pygame.sprite.Group()
+
+
+# Load options files
+def default():
+    print("Key not bound")
+
+
+def loadInputMap(path):
+    with open(path, "r") as f:
+        raw_file = json.load(f)
+
+    K_Map = {}
+    F_Map = {
+        "shoot": player.shoot,
+        "quit": sys.exit,
+    }
+    for key, action in raw_file.items():
+        K_Map[getattr(pygame, key)] = F_Map.get(action, default)
+    return K_Map
+
+
+configKeyMap = loadInputMap("options/keymaps.json")
 
 # Main Loop
 running = True
@@ -27,6 +50,9 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            action = configKeyMap.get(event.key, default)
+            action()
 
     screen.fill(BG)
     playerGroup.update(mousePos)
