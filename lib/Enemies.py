59ffe6d128d8e019ced335feef71788b401ceb53
fcpoloia <z21comma@proton.me>
2025-01-04 23:21:20 +0100

    Enemy base
    Added:
    -lib/Enemies.py - This will contain all enemies' classes
    -Dummy class - Inherits from pygame.sprite.Sprite. Base for all proceeding classes

---
+++ lib/Enemies.py
@@ -0,0 +1,36 @@
+import pygame
+import constants
+
+
+class Dummy(pygame.sprite.Sprite):
+    """
+    This is a base class for all enemies
+
+    PARAMS
+    path - A path to sprite's image
+    speed - number of pixel that a sprite will travel in one frame
+    health - self explenatory
+    """
+
+    def __init__(
+        self,
+        path: str,
+        speed: int,
+        health: int,
+    ) -> None:
+        super().__init__()
+
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.SPEED = speed
+        self.HP = health
+
+        self.PosVector = pygame.math.Vector2(self.rect.center)
+        dx = constants.center[0] - self.rect.centerx
+        dy = constants.center[1] - self.rect.centery
+        self.dirVector = pygame.Vector2(dx, dy).normalize()
+
+    def update(self):
+        self.PosVector += self.dirVector * self.SPEED
+        self.rect.centerx = round(self.PosVector.x)
+        self.rect.centery = round(self.PosVector.y)
